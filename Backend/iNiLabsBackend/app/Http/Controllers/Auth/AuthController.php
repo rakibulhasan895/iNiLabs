7cae335a9e6a43aaf512d258a39af8bc86caeb7d
rakibulhasan895 <rakibulhasan.rh895@gmail.com>
2025-12-07 13:35:25 +0600

    Authentication using sanctum

---
+++ Backend/iNiLabsBackend/app/Http/Controllers/Auth/AuthController.php
@@ -0,0 +1,52 @@
+<?php
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\LoginRequest;
+use App\Http\Requests\RegisterRequest;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
+use Symfony\Component\HttpFoundation\Response;
+
+class AuthController extends Controller
+{
+    public function register(RegisterRequest $request)
+    {
+        $validatedData = $request->all();
+        $user = User::create([
+            'name' => $validatedData['name'],
+            'email' => $validatedData['email'],
+            'password' => Hash::make($validatedData['password']),
+        ]);
+
+        return response()->json(['message' => 'Registered successfully'], Response::HTTP_CREATED);
+    }
+
+    public function login(LoginRequest $request)
+    {
+        $validatedData = $request->all();
+
+        $user = User::where('email', $validatedData['email'])->first();
+
+        if (! $user || ! Hash::check($validatedData['password'], $user->password)) {
+            throw ValidationException::withMessages([
+                'email' => ['The provided credentials are incorrect.'],
+            ]);
+        }
+
+        $token = $user->createToken('api-token')->plainTextToken;
+
+        return response()->json(['token' => $token]);
+    }
+
+    public function logout(Request $request)
+    {
+        if ($request->user()) {
+            $request->user()->currentAccessToken()->delete();
+        }
+
+        return response()->json(['message' => 'Logged out']);
+    }
+}
